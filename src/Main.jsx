ed06f973fae78b0373c14ac51e7814ad927c1162
meykiacs <meykiacs@gmail.com>
2023-12-19 13:17:30 +0330

    duplicate user for post

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,15 +1,18 @@
 import { Routes, Route } from "react-router-dom"
-import { Index } from "./pages/User/Index"
-import Create from "./pages/User/Create"
+import { Index as UserIndex } from "./pages/User/Index"
+import { Create as CreateUser } from "./pages/User/Create"
+import { Index as PostIndex } from "./pages/Post/Index"
+import { Create as CreatePost } from "./pages/Post/Create"
 
 export function Main() {
   return (
     <main>
       <Routes>
-        <Route path="users/create" element={<Create />} /> 
+        <Route path="users/create" element={<CreateUser />} /> 
         <Route path="users/:userId/edit" element={<div>edit</div>} /> 
-        <Route path="users" element={<Index />} />
-        <Route path="posts" element={<div>posts</div>} />
+        <Route path="users" element={<UserIndex />} />
+        <Route path="posts/create" element={<CreatePost />} /> 
+        <Route path="posts" element={<PostIndex />} />
         <Route path="settings" element={<div>settings</div>} />
       </Routes>
     </main>
