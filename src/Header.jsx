db7208ab556de9e5f8b8bf99c6e787276ae47eae
meykiacs <meykiacs@gmail.com>
2023-12-19 18:49:15 +0330

    update header and sidebar for dark mode

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,10 +2,10 @@ import { Bars3Icon, MagnifyingGlassIcon } from "@heroicons/react/20/solid"
 
 export function Header({ setSidebarOpen }) {
   return (
-    <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-6 bg-bg px-4 shadow-sm sm:px-6 lg:px-8">
+    <div className="bg-bg sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-6 px-4 shadow-sm sm:px-6 lg:px-8 dark:border-b dark:border-white/5">
       <button
         type="button"
-        className="-m-2.5 p-2.5 text-gray-700 xl:hidden"
+        className="-m-2.5 p-2.5 text-gray-700 xl:hidden dark:text-white"
         onClick={() => setSidebarOpen(true)}
       >
         <span className="sr-only">Open sidebar</span>
@@ -19,12 +19,12 @@ export function Header({ setSidebarOpen }) {
           </label>
           <div className="relative w-full">
             <MagnifyingGlassIcon
-              className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"
+              className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400 dark:text-gray-500"
               aria-hidden="true"
             />
             <input
               id="search-field"
-              className="block h-full w-full border-0 bg-transparent py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
+              className="block h-full w-full border-0 bg-transparent py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 dark:placeholder:text-gray-500 sm:text-sm dark:text-white"
               placeholder="Search..."
               type="search"
               name="search"
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -4,13 +4,6 @@ import {
   ServerIcon,
 } from "@heroicons/react/24/outline"
 
-const teams = [
-  { id: 1, name: "Planetaria", href: "#", initial: "P", current: false },
-  { id: 2, name: "Protocol", href: "#", initial: "P", current: false },
-  { id: 3, name: "Tailwind Labs", href: "#", initial: "T", current: false },
-]
-
-import { classNames } from "./utils"
 import { NavLink } from "react-router-dom"
 import { ThemeToggle } from "./ThemeToggle"
 
@@ -22,7 +15,7 @@ const navigation = [
 
 export function Sidebar() {
   return (
-    <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-bg px-6 ">
+    <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-bg px-6 border-e border-gray-200 dark:ring-1 dark:ring-white/10 dark:border-none">
       <div className="flex h-16 shrink-0 items-center">
         <img className="h-16 w-auto" src="/logo.svg" alt="Your Company" />
       </div>
@@ -37,8 +30,8 @@ export function Sidebar() {
                     to={item.href}
                     className={(navData) =>
                       navData.isActive
-                        ? "group flex gap-x-3 rounded-md bg-gray-50 p-2 text-sm font-semibold leading-6 text-indigo-600  hover:bg-gray-50 hover:text-indigo-600"
-                        : "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:bg-gray-50 hover:text-indigo-600"
+                        ? "group flex gap-x-3 rounded-md bg-gray-50 p-2 text-sm font-semibold leading-6 text-indigo-600  hover:bg-gray-50 hover:text-indigo-600 dark:bg-gray-800"
+                        : "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:bg-gray-50 hover:text-indigo-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-800"
                     }
                   >
                     <item.icon
@@ -51,42 +44,10 @@ export function Sidebar() {
               ))}
             </ul>
           </li>
-          <li>
-            <div className="text-xs font-semibold leading-6 text-gray-400">
-              Secondary nav
-            </div>
-            <ul className="-mx-2 mt-2 space-y-1">
-              {teams.map((team) => (
-                <li key={team.name}>
-                  <a
-                    href={team.href}
-                    className={classNames(
-                      team.current
-                        ? "bg-gray-50 text-indigo-600"
-                        : "text-gray-700 hover:bg-gray-50 hover:text-indigo-600",
-                      "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6",
-                    )}
-                  >
-                    <span
-                      className={classNames(
-                        team.current
-                          ? "border-indigo-600 text-indigo-600"
-                          : "border-gray-200 text-gray-400 group-hover:border-indigo-600 group-hover:text-indigo-600",
-                        "flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border bg-white text-[0.625rem] font-medium",
-                      )}
-                    >
-                      {team.initial}
-                    </span>
-                    <span className="truncate">{team.name}</span>
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </li>
           <li className="-mx-6 mt-auto">
             <a
               href="google.com"
-              className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6  text-gray-900 hover:bg-gray-50"
+              className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6  text-gray-900 hover:bg-gray-50 dark:text-white dark:hover:bg-gray-800"
             >
               <img
                 className="h-8 w-8 rounded-full bg-gray-50"
