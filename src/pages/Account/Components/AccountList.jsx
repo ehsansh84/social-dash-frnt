408a98b0654b1b182ae46c728b18b6279b7b268f
Mey Kia <meykiacs@gmail.com>
2023-12-23 23:09:26 +0330

    change ml- to ms-

--- src/pages/Account/Components/AccountList.jsx
+++ src/pages/Account/Components/AccountList.jsx
@@ -98,7 +98,7 @@ export function AccountList() {
                   Edit Account
                 </a>
               </div>
-              <div className="-ml-px flex w-0 flex-1">
+              <div className="-ms-px flex w-0 flex-1">
                 <a
                   href={`tel:${account.token}`}
                   className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
