ded51e8935aa282d3bca3beeb50e681824caf58f
meykiacs <meykiacs@gmail.com>
2023-12-25 17:27:34 +0330

    add style to account link in source list

--- src/pages/Source/Components/SourceList.jsx
+++ src/pages/Source/Components/SourceList.jsx
@@ -58,7 +58,7 @@ export function SourceList() {
                 <p className="truncate">{source.description}</p>
               </div>
               <Link
-                className="relative text-xs leading-5 text-gray-500 hover:text-gray-200"
+                className="relative text-xs leading-5 text-gray-500  underline hover:no-underline"
                 to={`/accounts/${source.account_id}/edit`}
               >
                 account id {source.account_id}
