465fa760bcf2aa765af584a37ce19567f89e9221
Mey Kia <meykiacs@gmail.com>
2023-12-27 23:12:52 +0330

    fix save and edit button for account

--- src/pages/Account/Create.jsx
+++ src/pages/Account/Create.jsx
@@ -191,9 +191,9 @@ export function Create() {
               type="submit"
               className="rounded-md bg-primary-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-primary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
             >
-              {createResourceMutation.isLoading ? (
+              {createResourceMutation.isPending ? (
                 <div
-                  className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                  className="inline-block h-4 w-4 mx-2 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
                   role="status"
                 >
                   <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
