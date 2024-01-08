6b86bd1c55a5325b07c0afde69966abd703cfcde
Mey Kia <meykiacs@gmail.com>
2024-01-08 08:53:54 +0330

    add badge component

---
+++ src/components/Badge.jsx
@@ -0,0 +1,15 @@
+export function Badge({ color = "gray", variant="somple", children, className="", ...delegated }) {
+  const baseClasses = variant === "pill" ?
+  "inline-flex items-center rounded-full px-2 py-1 text-xs font-medium ring-1 ring-inset" :
+    "inline-flex items-center rounded-md px-2 py-1 text-xs font-mediumring-1 ring-inset"
+  const colorClasses = {
+    gray: `${baseClasses} bg-gray-50 text-gray-600 ring-gray-500/10 dark:bg-gray-400/10 dark:text-gray-400 dark:ring-gray-400/20`,
+    red: `${baseClasses} bg-red-50 text-red-400 ring-red-400/20 dark:bg-red-400/10 dark:text-red-400 dark:ring-red-400/20`,
+    green: `${baseClasses} bg-green-50 text-green-700 ring-green-600/20 bg-green-500/10  text-green-400 ring-green-500/20`,
+  }
+  return (
+    <span className={`${colorClasses[color]} ${className}`} {...delegated}>
+      {children}
+    </span>
+  )
+}
