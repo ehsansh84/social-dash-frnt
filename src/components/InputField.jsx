8e38778b88ec52a613544c2258e31cd274b5bbf9
Mey Kia <meykiacs@gmail.com>
2024-01-10 22:37:22 +0330

    update inputfield

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,10 +1,4 @@
-export function InputField({
-  id,
-  label,
-  value,
-  setValue,
-  ...delegated
-}) {
+export function InputField({ id, label, value, setValue, ...delegated }) {
   return (
     <>
       <label
@@ -14,19 +8,17 @@ export function InputField({
         {label}
       </label>
       <div className="mt-2">
-        <div className="relative flex rounded-md shadow-sm ring-1 ring-inset ring-ring focus-within:ring-2 focus-within:ring-inset focus-within:ring-primary sm:max-w-md">
-          <input
-            value={value}
-            onChange={(e) => {
-              setValue(e.target.value)
-            }}
-            type="text"
-            name={id}
-            id={id}
-            className="block flex-1 border-0 bg-transparent py-1.5 ps-3 text-text placeholder:text-placeholder focus:ring-0 sm:text-sm sm:leading-6"
-            {...delegated}
-          />
-        </div>
+        <input
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value)
+          }}
+          type="text"
+          name={id}
+          id={id}
+          className="ring-inset ring-gray-300 block w-full flex-1 rounded-md border-0 bg-transparent py-1.5 ps-3 text-text shadow-sm ring-1 placeholder:text-placeholder focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
+          {...delegated}
+        />
       </div>
     </>
   )
