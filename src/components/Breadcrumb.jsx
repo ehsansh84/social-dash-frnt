78712def4359de020500e4f0863f8917f121d3db
meykiacs <meykiacs@gmail.com>
2024-01-11 13:45:23 +0330

    move breadcrumb divider into the breadcrumb component

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -1,5 +1,16 @@
 import { Link } from "react-router-dom"
-import { BreadcrumbDivider } from "./BreadcrumbDivider"
+
+const BreadcrumbDivider = () => (
+  <svg
+    className="ms-2 h-5 w-5 text-gray-300"
+    fill="currentColor"
+    viewBox="0 0 20 20"
+    aria-hidden="true"
+  >
+    <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
+  </svg>
+)
+
 
 export function Breadcrumb({ pages }) {
   return (
