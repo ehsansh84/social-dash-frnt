1a038e3e3be4db90b057030aeae05a62db0f2a90
Mey Kia <meykiacs@gmail.com>
2024-01-18 00:53:06 +0330

    update SectionHeading: fixing height

--- src/SectionHeadingWithActionButton.jsx
+++ src/SectionHeadingWithActionButton.jsx
@@ -1,6 +1,6 @@
 export function SectionHeadingWithActionButton({ children, Button }) {
   return (
-    <div className="py-5 sm:flex sm:items-center sm:justify-between">
+    <div className="py-5 sm:flex sm:items-center sm:justify-between h-16">
       <h3 className="text-base font-semibold leading-6 text-text">
         {children}
       </h3>
