50dd1c232f805d34da8eb2c73854510129491cba
meykiacs <meykiacs@gmail.com>
2024-01-19 19:24:13 +0330

    update sidebar nav for roles and permissions

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -5,20 +5,31 @@ import {
   UserIcon,
   DocumentIcon,
   UserCircleIcon,
-  ClockIcon
+  ClockIcon,
+  ChevronRightIcon,
 } from "@heroicons/react/24/outline"
 
 import { NavLink } from "react-router-dom"
 import { ThemeToggle } from "./components/ThemeToggle"
+import { Disclosure } from "@headlessui/react"
+import { classNames } from "./utils"
 
 const navigation = [
   { name: "Dashboard", href: "/", icon: PresentationChartLineIcon },
-  { name: "Users", href: "users", icon: UserIcon },
   { name: "Posts", href: "posts", icon: DocumentIcon },
   { name: "Sources", href: "sources", icon: FolderIcon },
   { name: "Destinations", href: "destinations", icon: FolderIcon },
   { name: "Accounts", href: "accounts", icon: UserCircleIcon },
   { name: "Schedules", href: "schedules", icon: ClockIcon },
+  {
+    name: "User Management",
+    icon: UserIcon,
+    children: [
+      { name: "Users", href: "users" },
+      { name: "Roles", href: "roles" },
+      { name: "Permissions", href: "permissions" },
+    ],
+  },
   { name: "Settings", href: "settings", icon: Cog6ToothIcon },
 ]
 
@@ -35,20 +46,64 @@ export function Sidebar() {
             <ul className="-mx-2 space-y-1">
               {navigation.map((item) => (
                 <li key={item.name}>
-                  <NavLink
-                    to={item.href}
-                    className={(navData) =>
-                      navData.isActive
-                        ? "group flex gap-x-3 rounded-md bg-gray-100 p-2 text-sm font-semibold leading-6 text-primary-600 hover:bg-bg-hover hover:text-primary-600 dark:bg-gray-800"
-                        : "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:bg-bg-hover hover:text-primary-600 dark:text-gray-400 dark:hover:text-white"
-                    }
-                  >
-                    <item.icon
-                      className="h-6 w-6 shrink-0"
-                      aria-hidden="true"
-                    />
-                    {item.name}
-                  </NavLink>
+                  {!item.children ? (
+                    <NavLink
+                      to={item.href}
+                      className={(navData) =>
+                        navData.isActive
+                          ? "group flex gap-x-3 rounded-md bg-gray-100 p-2 text-sm font-semibold leading-6 text-primary-600 hover:bg-bg-hover hover:text-primary-600 dark:bg-gray-800"
+                          : "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:bg-bg-hover hover:text-primary-600 dark:text-gray-400 dark:hover:text-white"
+                      }
+                    >
+                      <item.icon
+                        className="h-6 w-6 shrink-0"
+                        aria-hidden="true"
+                      />
+                      {item.name}
+                    </NavLink>
+                  ) : (
+                    <Disclosure as="div">
+                      {({ open }) => (
+                        <>
+                          <Disclosure.Button className="group flex w-full items-center gap-x-3 rounded-md p-2 text-left text-sm font-semibold leading-6 text-gray-700 hover:bg-bg-hover hover:text-primary-600 dark:text-gray-400 dark:hover:text-white">
+                            <item.icon
+                              className="h-6 w-6 shrink-0 text-gray-400"
+                              aria-hidden="true"
+                            />
+                            {item.name}
+                            <ChevronRightIcon
+                              className={classNames(
+                                open
+                                  ? "rotate-90 "
+                                  : "",
+                                "ml-auto h-5 w-5 shrink-0",
+                              )}
+                              aria-hidden="true"
+                            />
+                          </Disclosure.Button>
+                          <Disclosure.Panel as="ul" className="mt-1 px-2">
+                            {item.children.map((subItem) => (
+                              <li key={subItem.name}>
+                                <NavLink
+                                  to={subItem.href}
+                                  className={(navData) => 
+                                    classNames(
+                                      "block rounded-md py-2 pl-9 pr-2 text-sm leading-6", 
+                                        navData.isActive ? 
+                                        "bg-gray-100 text-primary-600 hover:bg-bg-hover hover:text-primary-600 dark:bg-gray-800"
+                                        : " text-gray-700 hover:bg-bg-hover hover:text-primary-600 dark:text-gray-400 dark:hover:text-white"
+                                        )
+                                      }
+                                      >
+                                  {subItem.name}
+                                </NavLink>
+                              </li>
+                            ))}
+                          </Disclosure.Panel>
+                        </>
+                      )}
+                    </Disclosure>
+                  )}
                 </li>
               ))}
             </ul>
