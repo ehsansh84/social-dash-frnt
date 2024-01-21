e83dedfa3d21e6987fca6e68e0ff02306d038184
Mey Kia <meykiacs@gmail.com>
2024-01-22 01:12:53 +0330

    update heading ui

--- src/SectionHeadingWithActionButton.jsx
+++ src/SectionHeadingWithActionButton.jsx
@@ -1,6 +1,6 @@
 export function SectionHeadingWithActionButton({ children, Button }) {
   return (
-    <div className="py-5 sm:flex sm:items-center sm:justify-between h-16">
+    <div className="py-5 space-y-3 sm:flex sm:items-center sm:justify-between">
       <h3 className="text-base font-semibold leading-6 text-text">
         {children}
       </h3>
