02d519a00065c04de81621384612fe5f6cf0654d
meykiacs <meykiacs@gmail.com>
2024-02-10 17:27:46 +0330

    add dataUrl to file utility function

--- src/utils.js
+++ src/utils.js
@@ -18,21 +18,21 @@ export function validateUsername(text) {
   return {
     test: () => textPattern.test(text),
     instructions: [
-      '4 to 24 characters.',
-      'Must begin with a letter',
-      'Letters, numbers, underscores. hyphons allowed.'
-    ]
+      "4 to 24 characters.",
+      "Must begin with a letter",
+      "Letters, numbers, underscores. hyphons allowed.",
+    ],
   }
 }
 
 export function validatePassword(text) {
-  const textPattern =  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
+  const textPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
   return {
     test: () => textPattern.test(text),
     instructions: [
-      '8 to 24 characters.',
-      'Must include uppercase and lowercase letters, a number and a special character.'
-    ]
+      "8 to 24 characters.",
+      "Must include uppercase and lowercase letters, a number and a special character.",
+    ],
   }
 }
 
@@ -44,5 +44,17 @@ export const isPhoneValid = (phone) => {
   } catch (error) {
     return false
   }
+}
+
+export function dataURLtoFile(dataurl, filename) {
+  let arr = dataurl.split(","),
+    mime = arr[0].match(/:(.*?);/)[1],
+    bstr = atob(arr[1]),
+    n = bstr.length,
+    u8arr = new Uint8Array(n)
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n)
+  }
+  return new File([u8arr], filename, { type: mime })
 }
 
